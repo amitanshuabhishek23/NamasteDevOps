9ab0aed3f9854d3c5e4bf3f974d1c89788824eed
Amitanshu <amitanshuabhishek17@gmail.com>
2023-09-02 09:16:10 +0530

    Add all

--- Shell Scripting/System_Report/mem_usage.sh
+++ Shell Scripting/System_Report/mem_usage.sh
@@ -3,3 +3,10 @@
     Author : amitanshu Abhishek
     Date : 20th Aug 2023
 '
+
+function mem_stats {
+    echo "........................................"
+    echo "..........MEMORY USAGE.............."
+    echo "........................................"
+    ps -eocomm,pmem | grep -E -v '(0.0)' >> "$1".txt
+}
